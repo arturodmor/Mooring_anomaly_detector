0520cba00fbf5e33c5fbca0b1cd7320c523313bc
arturodmor <rodriguezmoranarturo@gmail.com>
2024-08-27 19:10:15 +0100

    [PACKAGE] Update MODELS class. Now is oriented to transform data and return metrics. I considered that is not usefull try to include all ML models inside

--- anchoring_detection/models.py
+++ anchoring_detection/models.py
@@ -6,31 +6,26 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn import svm
-from sklearn.linear_model import SGDClassifier
-from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
-
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import GridSearchCV
 
 import matplotlib.pyplot as plt
 from mlxtend.plotting import plot_confusion_matrix
 from sklearn.inspection import DecisionBoundaryDisplay
+
+import logging
+
+logger = logging.Logger(__name__)
 
 
 class Models(Preprocess):
 
     "Class to train and evaluate models"
 
-    def __init__(self, dataframes_surge=None, dataframes_sway=None, df_surge = None, df_sway = None, n_states=10, n_dirs=5):
-        super().__init__(movements=['Surge', 'Sway'], main_path=rf'D:\arturo_sim\simulaciones\acoplado', dataframes_surge=dataframes_surge, dataframes_sway=dataframes_sway)
-        self.df_surge = df_surge
-        self.df_sway = df_sway
-        self.n_states = n_states
-        self.n_dirs =n_dirs
+    def __init__(self, cm = None, df_metrics = None):
+        super().__init__()
+        self.cm = cm
+        self.df_metrics = df_metrics
 
     
     def transform_dataset(self,dataset):
@@ -53,98 +48,42 @@ class Models(Preprocess):
         X_test_min_max = min_max_scaler.transform(X_test)
 
         return X_train_min_max, X_test_min_max, Y_train, Y_test
-    
 
-    def evaluate_models(self,model='svm'):
 
-        """ To evaluate models before and after hyperparams setup
+    def return_metrics(self,Y = None, Y_pred = None):
+
+        """To return training metrics
+
+        Args:
+            Y(Object): Train or test label set from the dataset
+            Y_pred(Object): Predictions
 
         Returns:
-            Metrics and cross validation results. Dataframe with Hyperparams results
+            cm(Object): COnfusión matrix
+            df_metrics(Dataframe): Dataframe with considered metrics
         """
 
-        self.transform_dataset()
+        try:
+            # Metrics
+            accuracy = metrics.accuracy_score(Y,Y_pred)
+            precision = metrics.precision_score(Y,Y_pred)
+            recall = metrics.recall_score(Y,Y_pred)
+            f1 = metrics.f1_score(Y,Y_pred)
+            self.cm = metrics.confusion_matrix(Y,Y_pred)
 
-        if model == "svm":
-            clf = svm.SVC()
+            data = {
+                'accuracy': [accuracy],
+                'precision': [precision],
+                'recall': [recall],
+                'f1': [f1]
+            }
 
-            param_grid = [
-                {'C':[1, 10, 100, 1000],'kernel':['linear']},
-                {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
-                {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['sigmoid']}
-                ]
-            
-            metric_columns = ['param_C', 'param_gamma', 'param_kernel', 'mean_train_accuracy', 'mean_train_precision', 'mean_train_recall', 'mean_train_f1']
+            self.df_metrics = pd.DataFrame(data)
 
-        elif model =="logistic":
-            clf = LogisticRegression()
-
-            param_grid = [
-                {'C':[1, 10, 50, 100],'penalty':['l1','l2'],'solver':['liblinear']}
-                ]
-            
-            metric_columns = ['param_C', 'param_penalty', 'param_solver', 'mean_train_accuracy', 'mean_train_precision', 'mean_train_recall', 'mean_train_f1']
-
+            return self.cm, self.df_metrics
         
-        clf.fit(X_train_min_max,Y_train)
-
-        #Cross Validation
-        y_validate_scores = cross_val_score(clf, X_train_min_max, Y_train, cv=5,scoring='accuracy')
-        y_validate_pred = cross_val_predict(clf, X_train_min_max, Y_train, cv=5)
-
-        #Metrics
-        accuracy = metrics.accuracy_score(Y_train,y_validate_pred)
-        precision = metrics.precision_score(Y_train,y_validate_pred)
-        recall = metrics.recall_score(Y_train,y_validate_pred)
-        f1 = metrics.f1_score(Y_train,y_validate_pred)
-
-        cm = metrics.confusion_matrix(Y_train,y_validate_pred)
-
-        print(y_validate_scores, y_validate_pred, accuracy, precision, recall, f1, cm)
-    
-        #Hyperparams selection
-        grid_search = GridSearchCV(clf, param_grid,  cv=5, scoring= ['accuracy','precision','recall','f1'],refit='accuracy', return_train_score=True)
-        grid_search.fit(X_train_min_max,Y_train)
-
-        hyperparams = pd.DataFrame(grid_search.cv_results_)
-        hyperparams = hyperparams[metric_columns]
-
-        return hyperparams
-
-
-    def train_models(self,model='svm'):
-
-        """ To train models and do prediction with test set
-
-        Returns:
-            Results with test set
-        """
-
-        if model == "svm":
-            clf = svm.SVC(C=1, kernel='linear')
-
-        elif model =="logistic":
-            clf = LogisticRegression(C=1, penalty='l2',solver='liblinear')
-
-        #Train and predict
-        clf.fit(X_train_min_max,Y_train)
-        Y_pred_test = clf.predict(X_test_min_max)
-
-        #Test metrics
-        accuracy_test = metrics.accuracy_score(Y_test,Y_pred_test)
-        precision_test = metrics.precision_score(Y_test,Y_pred_test)
-        recall_test = metrics.recall_score(Y_test,Y_pred_test)
-        f1_test = metrics.f1_score(Y_test,Y_pred_test)
-        cm_test = metrics.confusion_matrix(Y_test,Y_pred_test)
-
-        columns = ['acurracy', 'precision','recall','f1','matriz_confusión']
-        test_metrics=pd.DataFrame(columns=columns,data=[accuracy_test,precision_test,recall_test,f1_test,cm_test])
-
-        return test_metrics
-
-
-
-
+        except Exception as e:
+            logger.error('[ERROR] Params are None. Remember to define them')
 
 
 
